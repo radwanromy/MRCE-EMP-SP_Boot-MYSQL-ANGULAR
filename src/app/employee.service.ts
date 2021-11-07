5d162fa61a1ba10d6ab595eb47e2ae09ac623d9f
radwanromy <radwanromy@gmail.com>
2021-11-07 17:21:23 +0600

    Angular TypeScript 111

--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Employee } from './employee';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
 
-  constructor() { }
+  private baseURL = "http://localhost:8000/api/v1/employees";
+
+  constructor(private httpClient: HttpClient) { }
+
+  getEmployeesList() : Observable<Employee[]>{
+    return this.httpClient.get('${this.baseURL}');
+  }
 }

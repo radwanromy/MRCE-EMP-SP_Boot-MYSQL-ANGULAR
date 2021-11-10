0893f43c7ad426363e4d134803e920e308ba45a9
radwanromy <radwanromy@gmail.com>
2021-11-10 15:55:15 +0600

    Angular TypeScript 116

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'employees',component: EmployeeListComponent},
+  {path: '',redirectTo: 'employees', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
